42b4ca7ad5e562a904b6d864cf036280e5d065b6
Abdul Hannan <abdulogs>
2023-07-19 10:06:56 +0500

    Fixes

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,9 +1,9 @@
-// window.onscroll = function () { scrollFunction() };
+window.onscroll = function () { scrollFunction() };
 
-// function scrollFunction() {
-//     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//         document.getElementById("navbar").classList.add("bg-dark");
-//     } else {
-//         document.getElementById("navbar").classList.remove("bg-dark");
-//     }
-// }
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("navbar").classList.add("bg-dark");
+    } else {
+        document.getElementById("navbar").classList.remove("bg-dark");
+    }
+}
