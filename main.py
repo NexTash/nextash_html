21e7f92a25e288c958369fc09d8fc11903cc1b24
abdulogs <abdulogs88@gmail.com>
2023-07-25 10:37:52 +0500

    Fixes

--- main.py
+++ main.py
@@ -4,50 +4,55 @@ from utils import if_file_exist
 
 app = Flask(__name__, template_folder="public", static_folder="src")
 
+# Route - Page - Function
 routes = [
-    "/:index:index",
-    "/contact-us:contact_us:contact-us",
-    "/about-us:about_us:about-us",
-    "/faqs:faqs:faqs",
-    "/team:team:expert-team",
-    "/privacy-policy:privacy_policy:privacy-policy",
-    "/terms-and-conditions:terms_and_conditions:terms-and-conditions",
-    "/cookies-policy:cookies_policy:cookies-policy",
-    "/blogs:blogs:blogs",
-    "/blog-details:blog_details:blog-details",
-    "/news:news:news",
-    "/news-details:news_details:news-details",
-    "/jobs:jobs:jobs",
-    "/job-details:job_details:job-details",
-    "/projects:projects:projects",
-    "/product-details:project_detais:product-details",
-    "/services:services:services",
-    "/service/branding:service_branding:service-branding",
-    "/service/ui-ux:service_graphics_design:service-ui-ux",
-    "/service/web-development:service_web_development:service-web-development",
-    "/service/mobile-app-development:service_app_development:service-mobile-app-development",
-    "/service/digital-marketing:service_digital_marketing:service-digital-marketing",
-    "/service/cyber-security:service_cyber_security:service-cyber-security",
-    "/service/devops:service_devops:service-devops",
-    "/service/networking:service_networking:service-networking",
-    "/service/erp-development:service_erp_development:service-erp-development",
-    "/service/database-administration:service_database_admininstration:service-database-administration",
-    "/service/server-administration:service_server_administration:service-server-administration",
-    "/service/cloud-management:service_cloud_management:service-cloud-management",
-    "/service/manage-hosting:service_manage_hosting:service-manage-hosting",
-    "/service/technical-support:service_technical_support:service-technical-support",
-    "/service/business-analysis:service_business_analysis:service-business-analysis"
+    {"page": "index", "url": ""},
+    {"page": "about-us", "url": "about-us"},
+    {"page": "contact-us", "url": "contact-us"},
+    {"page": "expert-team", "url": "expert-team"},
+    {"page": "faqs", "url": "faqs"},
+    {"page": "terms-and-conditions", "url": "terms-and-conditions"},
+    {"page": "privacy-policy", "url": "privacy-policy"},
+    {"page": "cookies-policy", "url": "cookies-policy"},
+    {"page": "blogs", "url": "blogs"},
+    {"page": "blog-details", "url": "blog-details"},
+    {"page": "news", "url": "news"},
+    {"page": "news-details", "url": "news-details"},
+    {"page": "jobs", "url": "jobs"},
+    {"page": "job-details", "url": "job-details"},
+    {"page": "projects", "url": "projects"},
+    {"page": "project-details", "url": "project-details"},
+    {"page": "products", "url": "products"},
+    {"page": "product-details", "url": "product-details"},
+    {"page": "services", "url": "services"},
+    {"page": "service-mobile-app-development", "url": "service-mobile-app-development"},
+    {"page": "service-web-development", "url": "service-web-development"},
+    {"page": "service-erp-development", "url": "service-erp-development"},
+    {"page": "service-branding", "url": "service-branding"},
+    {"page": "service-digital-marketing", "url": "service-digital-marketing"},
+    {
+        "page": "service-database-administration",
+        "url": "service-database-administration",
+    },
+    {"page": "service-server-administration", "url": "service-server-administration"},
+    {"page": "service-cyber-security", "url": "service-cyber-security"},
+    {"page": "service-networking", "url": "service-networking"},
+    {"page": "service-manage-hosting", "url": "service-manage-hosting"},
+    {"page": "service-cloud-management", "url": "service-cloud-management"},
+    {"page": "service-technical-support", "url": "service-technical-support"},
+    {"page": "service-business-analysis", "url": "service-business-analysis"},
+    {"page": "service-devops", "url": "service-devops"},
 ]
 
-for row in routes:
-    route, func_name, url = row.split(":")
-    if if_file_exist(url):
-            exec(f"""
-@app.route('{route}')
-def {func_name}():
-    return render_template('pages/{url}.html')
+for route in routes:
+    if if_file_exist(route["page"]):
+        function = f"""
+@app.route('/{route["url"]}')
+def {route["page"].replace("-","_")}():
+    return render_template('pages/{route["page"]}.html')
 """
-        )
+        exec(function)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -60,9 +65,10 @@ def server_error(e):
     # note that we set the 500 status explicitly
     return render_template("pages/500.html"), 500
 
+
 if __name__ == "__main__":
-    app.config['FREEZER_DESTINATION'] = 'docs'  # Output folder for frozen files
-    freezer = Freezer(app)
-    freezer.freeze()  # Generate static files
-    
+    # app.config["FREEZER_DESTINATION"] = "docs"  # Output folder for frozen files
+    # freezer = Freezer(app)
+    # freezer.freeze()  # Generate static files
+
     app.run(debug=True, port=8080)
